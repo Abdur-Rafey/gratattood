ac487314ebedb420e0bb7f629b4baa456db86b5b
Abdur Rafey <abdurrafey63@gmail.com>
2017-10-23 15:31:53 +0500

    Abdur Rafey 23/10/2017

---
+++ app/src/main/java/com/gratattood/gratattood/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.gratattood.gratattood.app;
+
+/**
+ * Created by Rafey Sheikh on 10/2/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://demo.byterace.com/gratattood/api/login_user.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://demo.byterace.com/gratattood/api/register_user.php";
+}
